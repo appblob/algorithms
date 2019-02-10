7bbebd4d8d01178132e4a044b0da0f94eb2e169d
girish nayak <girishlnk@gmail.com>
2019-02-09 16:28:07 -0800

    New checkin - 02/10/2019

--- src/linkedlist/AddTwoNumbers.java
+++ src/linkedlist/AddTwoNumbers.java
@@ -18,19 +18,6 @@ public class AddTwoNumbers {
      * add the carry to the result.
      *
      * */
-
-    public static void main(String[] args) {
-        AddTwoNumbers atn = new AddTwoNumbers();
-        int[] nums1 = {7, 8, 9};
-        int[] nums2 = {5, 4, 3};
-        Node l1 = Node.insert(nums1);
-        Node l2 = Node.insert(nums2);
-
-        Node l3 = atn.add(l1, l2);
-        Node.print(l3);
-
-    }
-
     public Node add(Node l1, Node l2) {
         Node dummy = new Node(0);
         Node iter = dummy;
@@ -66,5 +53,16 @@ public class AddTwoNumbers {
         return dummy.next;
     }
 
+    public static void main(String[] args) {
+        AddTwoNumbers atn = new AddTwoNumbers();
+        int[] nums1 = {7, 8, 9};
+        int[] nums2 = {5, 4, 3};
+        Node l1 = Node.insert(nums1);
+        Node l2 = Node.insert(nums2);
+
+        Node l3 = atn.add(l1, l2);
+        Node.print(l3);
+
+    }
 
 }
