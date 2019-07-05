94e971db1725b409d67f65a310c4b0e052427778
girish nayak <mails2girish@gmail.com>
2019-07-04 21:37:32 -0700

    Searching in rotated array

--- src/arrays/sortedarrays/binarysearch/SearchInRotatedSortedArray.java
+++ src/arrays/sortedarrays/binarysearch/SearchInRotatedSortedArray.java
@@ -26,39 +26,35 @@ public class SearchInRotatedSortedArray {
 
         int[] nums = {7, 8, 1, 2, 3, 4, 5};
         SearchInRotatedSortedArray sirsa = new SearchInRotatedSortedArray();
-        System.out.println("Index of 7 : " + sirsa.search(nums, 7));
-        System.out.println("Index of 4 : " + sirsa.search(nums, 4));
-        System.out.println("Index of 5 : " + sirsa.search(nums, 5));
-        System.out.println("Index of 6 : " + sirsa.search(nums, 6));
-        System.out.println("Index of 1 : " + sirsa.search(nums, 1));
-        System.out.println("Index of 8 : " + sirsa.search(nums, 8));
+        System.out.println("Index of 7 : " + search(nums, 7));
+        System.out.println("Index of 4 : " + search(nums, 4));
+        System.out.println("Index of 5 : " + search(nums, 5));
+        System.out.println("Index of 6 : " + search(nums, 6));
+        System.out.println("Index of 1 : " + search(nums, 1));
+        System.out.println("Index of 8 : " + search(nums, 8));
     }
 
-    public int search(int[] nums, int target) {
-        int l = 0, r = nums.length;
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
 
-        while (l != r) {
+            if(nums[mid] < nums[right]) {//[7 8 3 5 6]
+                if(target > nums[mid] && target <= nums[right]) left = mid + 1; //[7 8 3 5 6], 5
+                else right = mid - 1; //[7 8 3 5 6], 8
 
-            int m = l + (r - l) / 2;
+            } else if(nums[mid] > nums[right]) {//[6 7 8 3 5]
 
-            if (nums[m] == target)
-                return m;
-
-            // if left half is sortedarrays
-            if (nums[l] <= nums[m]) {
-                // check if target lies on the left half
-                if (nums[l] <= target && target < nums[m]) r = m;
-                else l = m + 1;
-
-            } else {
-
-                // check if target lies on the right half
-                if (nums[m] < target && target <= nums[r - 1]) l = m + 1;
-                else r = m;
+                if(target < nums[mid] && target >= nums[left]) right = mid - 1; //[6 7 8 3 5], 7
+                else left = mid + 1; //[6 7 8 3 5], 3
 
             }
-        }
+            else right--; // nums[mid] = nums[right] [1 2 3 3 3]
 
+        }
         return -1;
     }
+
 }
--- src/dynamicprogramming/MinNumOfCoinsToMakeTargetAmount.java
+++ src/dynamicprogramming/MinNumOfCoinsToMakeTargetAmount.java
@@ -1,11 +1,83 @@
 package dynamicprogramming;
 
+import java.util.Arrays;
+
 public class MinNumOfCoinsToMakeTargetAmount {
     /*
+    * Leetcode : 322
+    *
     * What is the minimum number of coins to make the target amount assuming there are unlimited number of coins.
     * */
-    public static int minimumCoins(int[] coins, int tgtAmount) {
-        return 0;
+
+
+    /*
+    coins = {1,5,10,25,50,100}, amount = 39
+
+    THOUGHT : for amt = 0 to amount calculate how many min coins are needed
+    */
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0) return -1;
+
+        // count the min no of coins required to find each amt from 0 to amount
+        int[] count = new int[amount + 1];
+
+        // 0 amt 0 coins
+        count[0] = 0;
+
+        // for each amt find how many coins are required. 1 <= amt <= amount
+        for(int amt = 1; amt <= amount; amt++) {
+
+            int minCoins = Integer.MAX_VALUE;
+
+            // for each amt see how many coins are needed
+            for(int coin : coins) {
+
+                // if denomination of the coin is more than amount
+                if(coin > amt) continue;
+
+                // coins need to form 15 cents with 10 cents = 1 + coins need to form (15 - 10) = 5 cents
+                int prevCoinCount = count[amt - coin];
+
+                // the amount cannot be formed with current coin, just pick next coin
+                if(prevCoinCount == -1) continue;
+
+                minCoins = Math.min(minCoins, 1 + prevCoinCount);
+            }
+
+            // amount cannot be formed
+            if(minCoins == Integer.MAX_VALUE) minCoins = -1;
+
+            count[amt] = minCoins;
+        }
+
+        return count[amount];
+    }
+
+    /*
+    THOUGHT: greedy solution
+
+    Start with the highest denomination and walk backwards.
+    Each time update count as amount / coins[iterator] and
+    amount %= coins[iterator]
+    */
+    public int coinChangeV0(int[] coins, int amount) {
+
+        if(coins == null || coins.length == 0) return -1;
+
+        int count = 0;
+        int iterator = coins.length - 1;
+
+        Arrays.sort(coins);
+
+        while(amount > 0 && iterator >= 0) {
+
+            count += amount / coins[iterator];
+            amount %= coins[iterator];
+            iterator--;
+        }
+
+        return (amount == 0) ? count : -1;
+
     }
 
 }
