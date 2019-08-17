3be4104f1a1026dbae40db356eb78dfb3f44de7c
girish nayak <girish.l.nayak@gmail.com>
2019-08-17 11:44:46 -0700

    Contiguous Sum in a array of positive numbers that add up to X

--- src/arrays/subarray/SubArrayThatSumsToX.java
+++ src/arrays/subarray/SubArrayThatSumsToX.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class SubArrayThatSumsToX {
     /*
-    * Array of positive numbers. sum of continuous numbers
-    *
-    * Technique : 2 Pointers technique. Windowing.
-    * Since array contains only positive numbers, we can define a window and add it's contents and compare with X
-    * Start both left and right pointers at 0
-    * Note that the sum increases if we move right pointer and decreases if we move left pointer to right
-    *
-    * */
+     * Array of positive numbers. sum of continuous numbers
+     *
+     * Technique : 2 Pointers technique. Windowing.
+     * Since array contains only positive numbers, we can define a window and add it's contents and compare with X
+     * Start both left and right pointers at 0
+     * Note that the sum increases if we move right pointer and decreases if we move left pointer to right
+     *
+     * */
 
     public static List<Integer> contiguousSum(int[] arr, int X) {
         int left = 0, right = 0;
@@ -20,13 +20,13 @@ public class SubArrayThatSumsToX {
         int sum = 0;
 
         // explore all elements
-        while(left < arr.length) {
+        while (left < arr.length) {
 
-            if(sum == X) {
+            if (sum == X) {
 
                 return Arrays.asList(left, (right != 0) ? right - 1 : 0);
 
-            } else if(sum > X) {
+            } else if (sum > X) {
 
                 sum -= arr[left];
                 left++;
@@ -34,7 +34,7 @@ public class SubArrayThatSumsToX {
             } else {
 
                 // make sure right does not cross the boundary
-                if(right < arr.length) {
+                if (right < arr.length) {
 
                     sum += arr[right];
                     right++;
@@ -47,7 +47,7 @@ public class SubArrayThatSumsToX {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,50};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 50};
         System.out.println(contiguousSum(arr, 50));
         System.out.println(contiguousSum(arr, 1));
         System.out.println(contiguousSum(arr, 65));
